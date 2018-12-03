c3c24dc0c4d74122123222505433dfadb82215a6
Stephan Ahlf <stephan.ahlf@googlemail.com>
2018-12-03 12:00:24 +0100

    resolve 2nd task to return filtered and sorted posts

---
+++ test/route.posts.spec.js
@@ -0,0 +1,21 @@
+require('should')
+
+describe.only('#/posts', function () {
+	it('should get posts sorted by given property asc', async function () {
+		const res = await HTTP().get('/posts/?sortOrderProperty=title')
+		res.data.length.should.be.above(0)
+		const firstItem = res.data[0].title
+		const lastItem = res.data[res.data.length - 1].title
+
+		firstItem.should.not.be.above(lastItem)
+	})
+
+	it('should get posts sorted by given property desc', async function () {
+		const res = await HTTP().get('/posts/?sortOrderProperty=-title')
+		res.data.length.should.be.above(0)
+		const firstItem = res.data[0].title
+		const lastItem = res.data[res.data.length - 1].title
+
+		firstItem.should.be.above(lastItem)
+	})
+})
